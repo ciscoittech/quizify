d3eba97aca7f8658f17292e2e3f995e5447c32ef
terrence Battlehunt <ciscoittech@gmail.com>
2023-10-16 20:14:50 -0400

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,40 @@
 from flask import Flask, session
 from flask_login import LoginManager
-import mongoengine as me 
+import mongoengine as me
 import certifi
-from app.auth.routes import auth
-from app.admin.routes import admin  # Adjust this line
-from app.quiz.routes import quiz
 from config import Config
 import os
 
-app = Flask(__name__)
-app.register_blueprint(auth)
-app.register_blueprint(admin)  # Adjusted to the correct blueprint
-app.register_blueprint(quiz)
-app.config.from_object(Config)
+# Move this to a separate function
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-# Connect to the MongoDB database using mongoengine
+    # Blueprints registration
+    from app.auth.routes import auth
+    from app.admin.routes import admin
+    from app.quiz.routes import quiz
+    app.register_blueprint(auth)
+    app.register_blueprint(admin)
+    app.register_blueprint(quiz)
 
+    # Database connection
+    DB_URI = os.environ.get('DB_URI')
+    me.connect(host=DB_URI, tlsCAFile=certifi.where())
 
-DB_URI = os.environ.get('DB_URI')
-me.connect(host=DB_URI, tlsCAFile=certifi.where())
-# connect('demoquiz', host='localhost', port=27017)
+    # Flask-login configuration
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'auth.login'  #
+    @login_manager.user_loader
+    def load_user(user_id):
+        from app.admin.models import User
+        try:
+            return User.objects.get(id=user_id)
+        except me.DoesNotExist:
+            return None
 
+    return app
 
-@login_manager.user_loader
-def load_user(user_id):
-    from app.admin.models import User  # Import the User model here to avoid circular imports
-    try:
-        return User.objects.get(id=user_id)
-    except me.DoesNotExist:
-        return None
+app = create_app()
