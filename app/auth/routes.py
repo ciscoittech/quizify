df1434d56fa9bb6aefdfcc59bcf9b6bced7e8d07
terrence Battlehunt <ciscoittech@gmail.com>
2023-11-21 21:49:41 -0500

    Configured Oauth

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template, redirect, request, session, url_for, flash
+import secrets, requests
+from urllib.parse import urlencode
+from flask import abort, current_app, render_template, redirect, request, session, url_for, flash
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user
 from app.auth.forms import RegistrationForm
@@ -27,7 +29,7 @@ def login():
             next_page = url_for('admin.profile')
         return redirect(next_page)
 
-    return render_template('accounts/login.html', title='Sign In', form=form)
+    return render_template('accounts/loginoauth.html', title='Sign In', form=form)
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -69,3 +71,91 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('auth.login'))
+
+
+
+@bp.route('/authorize/<provider>')
+def oauth2_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('admin.profile'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # generate a random string for the state parameter
+    session['oauth2_state'] = secrets.token_urlsafe(16)
+
+    # create a query string with all the OAuth2 parameters
+    qs = urlencode({
+        'client_id': provider_data['client_id'],
+        'redirect_uri': url_for('auth.oauth2_callback', provider=provider,
+                                _external=True),
+        'response_type': 'code',
+        'scope': ' '.join(provider_data['scopes']),
+        'state': session['oauth2_state'],
+    })
+
+    # redirect the user to the OAuth2 provider authorization URL
+    return redirect(provider_data['authorize_url'] + '?' + qs)
+
+
+
+@bp.route('/callback/<provider>')
+def oauth2_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    # if there was an authentication error, flash the error messages and exit
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}')
+        return redirect(url_for('index'))
+
+    # make sure that the state parameter matches the one we created in the
+    # authorization request
+    if request.args['state'] != session.get('oauth2_state'):
+        abort(401)
+
+    # make sure that the authorization code is present
+    if 'code' not in request.args:
+        abort(401)
+
+    # exchange the authorization code for an access token
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('auth.oauth2_callback', provider=provider,
+                                _external=True),
+    }, headers={'Accept': 'application/json'})
+    if response.status_code != 200:
+        abort(401)
+    oauth2_token = response.json().get('access_token')
+    if not oauth2_token:
+        abort(401)
+
+    # use the access token to get the user's email address
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    if response.status_code != 200:
+        abort(401)
+    email = provider_data['userinfo']['email'](response.json())
+
+    # find or create the user in the database
+    user = User.objects(email=email).first()
+    if user is None:
+        user = User(email=email, username=email.split('@')[0])
+        user.save()  # MongoEngine's save method
+
+    # log the user in
+    login_user(user)
+    return redirect(url_for('admin.profile'))
